1aa8312b6b37f6356b1d0457819a51abb658eef6
Danilopc <sr_danilo@hotmail.com>
2024-02-22 20:11:39 +0000

    fist commit

---
+++ components/HelloDiv.jsx
@@ -0,0 +1,20 @@
+function HelloDiv() {
+  return (
+  
+  <div>
+  <h1>Hello! My name is Danilo</h1>
+
+<ul>
+  <h2>Best things to do on sundays:</h2>
+  <li> Sleep </li>
+  <li> Sleep </li>
+  <li> Sleep </li>
+  <li> ZZZZzzzzZZZZZZZzzzz... </li>
+
+
+</ul>
+
+  </div>);
+}
+
+export default HelloDiv;
